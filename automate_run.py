365a56dcdf680aa83c313dc8b451f252d305d093
artha <U21101466@sharjah.ac.ae>
2025-09-14 01:24:47 +0400

    Delete automate_run.py

--- automate_run.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-from main import dashboard_data
-
-# Run the app to update the investment memory and dashboard json data during scheduled runs
-async def main():
-  data = await dashboard_data()
-  print("✅ dashboard_data() was executed")
-
-asyncio.run(main())
